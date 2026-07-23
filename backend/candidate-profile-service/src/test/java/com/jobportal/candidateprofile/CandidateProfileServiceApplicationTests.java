c29e426864e89da1439481dc40d0bfe26b7f82b2
Sanket-Gaware <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>
2026-07-23 17:27:00 +0530

    Backend Candidate Service Sructure

---
+++ backend/candidate-profile-service/src/test/java/com/jobportal/candidateprofile/CandidateProfileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobportal.candidateprofile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateProfileServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
